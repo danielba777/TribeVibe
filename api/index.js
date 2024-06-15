4208b2198a4e8a64d03d753d8571b72527025136
danielba777 <danielbauer230501@gmail.com>
2024-06-15 15:11:29 +0200

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+const app = express()
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js"
+import commentRoutes from "./routes/commentRoutes.js"
+import likeRoutes from "./routes/likeRoutes.js"
+import postRoutes from "./routes/postRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
+
+const port = 8800
+
+//middleware
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.use("/api/auth", authRoutes)
+app.use("/api/comments", commentRoutes)
+app.use("/api/likes", likeRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/users", userRoutes)
+
+app.listen(port, () => {
+    console.log(`API is running on port ${port}`)
+})
